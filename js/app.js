84fc507199ab1f92c960bcd37135c858b8ce82fb
Moisés <filhomoises1708@gmail.com>
2025-03-31 20:07:00 -0300

    feat: adicionar confirmação antes da devolução de jogos

--- js/app.js
+++ js/app.js
@@ -2,11 +2,18 @@ function alterarStatus(id) {
     let gameClicado = document.getElementById(`game-${id}`);
     let imagem = gameClicado.querySelector('.dashboard__item__img');
     let botao = gameClicado.querySelector('.dashboard__item__button');
+    let resposta;
 
     if (imagem.classList.contains("dashboard__item__img--rented")) {
-        imagem.classList.remove("dashboard__item__img--rented");
-        botao.classList.remove("dashboard__item__button--return");
-        botao.textContent = "Alugar";
+        resposta = confirm("Clique em 'Ok' para confirmar a devolução");
+
+        if (resposta === true) {
+            imagem.classList.remove("dashboard__item__img--rented");
+            botao.classList.remove("dashboard__item__button--return");
+            botao.textContent = "Alugar";
+        } else {
+            return;
+        }
     } else {
         imagem.classList.add("dashboard__item__img--rented");
         botao.classList.add("dashboard__item__button--return");
